9a2cd352c7a49f7841545611b4966a1a03414627
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-03-20 20:17:47 +0000

    Refactor scoping.
    Functions were moved inside the scope of the post request where they belong, thus eliminating the
    need for a global response variable.
    This also fixes a bug, where I was not able to handle two individual post request's errors simultaneously
    due to the fact that the myRes variable would have always been overriden before a previous request
    would be able to finish.
    By removing the global variable, each request is within its own scope, as it should be.

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ var cmd = '/usr/games/fortune | /usr/games/cowsay';
 var Constants = require(__dirname + '/Constants.js')
 
 var stlFilePath = "./3dFiles/original.stl";
-var myRes = null;
 
 app.get('/', function(req, res) {
     exec(cmd, function(error, stdout, stderr) {
@@ -27,7 +26,6 @@ app.get('/', function(req, res) {
 
 app.post('/image', upload.single('shapeJS_img'), function(req, res) {
     var uuidJS = uuid.v4();
-    myRes = res;
     // console.log(req.file);
 
     var options = {
@@ -39,74 +37,75 @@ app.post('/image', upload.single('shapeJS_img'), function(req, res) {
             script: Constants.JS_2D_TO_3D
         }
     }
+
     restler.post(Constants.UPDATE_SCENE_ENDPOINT, options).on('complete', function(response, body) {
         console.log();
         console.log(response);
         console.log();
-        //console.log(error);
+        // console.log(error);
         // console.log(body);
 
-        get3DModel(uuidJS, body);
+        get3DModel(body);
     });
-});
 
-function get3DModel(uuidJS, body) {
-    restler.get(Constants.SAVE_MODEL_CACHED_ENDPOINT + uuidJS, {
-        decoding: 'buffer'
-    }).on('complete', function(body) {
-        fs.writeFile(stlFilePath, body, function(err) {
-            if (err) {
-                return console.log(err);
-            }
-            runPythonFixer();
+    function get3DModel(body) {
+        restler.get(Constants.SAVE_MODEL_CACHED_ENDPOINT + uuidJS, {decoding: 'buffer'})
+        .on('complete', function(body) {
+            fs.writeFile(stlFilePath, body, function(err) {
+                if (err) {
+                    return console.log(err);
+                }
+                runPythonFixer();
+            });
         });
-    });
-}
+    }
 
-function runPythonFixer() {
-    // execute python script to fix xml
-    exec('cd 3dFiles; sudo blender -b -P import_decimate_export.py; cd ..', function(error, stdout, stderr) {
-        console.log('Running python script');
-            console.log(error);
-            console.log(stdout);
-            console.log(stderr);
-        if (error) {
-            console.log(error);
-            console.log(stdout);
-            console.log(stderr);
-        } else {
-            console.log("Success\n");
-        }
+    // execute python script for importing, decimating, and exporting
+    function runPythonFixer() {
+        exec('cd 3dFiles; sudo blender -b -P import_decimate_export.py; cd ..', function(error, stdout, stderr) {
+            console.log('Running python script');
+                console.log(error);
+                console.log(stdout);
+                console.log(stderr);
+            if (error) {
+                console.log(error);
+                console.log(stdout);
+                console.log(stderr);
+            } else {
+                console.log("Success\n");
+            }
 
-        runFbxConv();
-    });
-}
+            runFbxConv();
+        });
+    }
 
-function runFbxConv() {
     // execute fbx-conv
-    exec('../conversion-tools/fbx-conv/fbx-conv-lin64 -o g3db ./3dFiles/test.fbx ./3dFiles/test.g3db', function(error, stdout, stderr) {
-        console.log('Running fbx-conv');
-        if (error) {
-            console.log(error);
-            console.log(stderr);
-        } else {
-            console.log("Success\n");
-        }
-        sendModel();
-    });
-}
+    function runFbxConv() {
+        exec('../conversion-tools/fbx-conv/fbx-conv-lin64 -o g3db ./3dFiles/test.fbx ./3dFiles/test.g3db', function(error, stdout, stderr) {
+            console.log('Running fbx-conv');
+            if (error) {
+                console.log(error);
+                console.log(stderr);
+            } else {
+                console.log("Success\n");
+            }
+            sendModel();
+        });
+    }
 
-function sendModel() {
-    myRes.sendFile("./3dFiles/test.g3db", {root: __dirname}, function(err) {
-        if (err) {
-            console.log(err);
-            myRes.status(err.status || 500).end();
-        } else {
-            console.log('Sent: test.g3db');
-        }
-    });
-}
+    function sendModel() {
+        res.sendFile("./3dFiles/test.g3db", {root: __dirname}, function(err) {
+            if (err) {
+                console.log(err);
+                res.status(err.status || 500).end();
+            } else {
+                console.log('Sent: test.g3db');
+            }
+        });
+    }
+});
 
 app.listen(80, function() {
     console.log('Listening on Port 80');
 });
+
