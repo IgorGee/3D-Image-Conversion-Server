dc6913377618cb91334d42c241c112faa9e843fc
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-03-21 19:26:37 +0000

    Add export for stl files in addition to fbx.

--- 3dFiles/import_decimate_export.py
+++ 3dFiles/import_decimate_export.py
@@ -2,6 +2,7 @@ import bpy
 
 stl_file = 'original.stl'
 fbx_file = 'test.fbx'
+new_stl_file = 'test.stl'
 
 bpy.ops.import_mesh.stl(filepath=stl_file)
 
@@ -10,3 +11,4 @@ bpy.context.object.modifiers["Decimate"].decimate_type = 'DISSOLVE'
 bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Decimate")
 
 bpy.ops.export_scene.fbx(filepath=fbx_file)
+bpy.ops.export_mesh.stl(filepath=new_stl_file)
