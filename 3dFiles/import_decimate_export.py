01334e0580484e8462cc717e8c83b473b5b97ccd
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-03-23 16:54:10 +0000

    Modify decimation for maximum detail

--- 3dFiles/import_decimate_export.py
+++ 3dFiles/import_decimate_export.py
@@ -5,10 +5,16 @@ fbx_file = 'test.fbx'
 new_stl_file = 'test.stl'
 
 bpy.ops.import_mesh.stl(filepath=stl_file)
+ob = bpy.context.object
 
-bpy.ops.object.modifier_add(type='DECIMATE')
-bpy.context.object.modifiers["Decimate"].decimate_type = 'DISSOLVE'
-bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Decimate")
+while len(ob.data.polygons) > 8000:
+    bpy.ops.object.modifier_add(type='DECIMATE')
+    bpy.context.object.modifiers["Decimate"].ratio = 0.95
+    bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Decimate")
+
+print('Vertices:', str(len(ob.data.vertices)))
+print('Edges:', str(len(ob.data.edges)))
+print('Polygons:', str(len(ob.data.polygons)))
 
 bpy.ops.export_scene.fbx(filepath=fbx_file)
 bpy.ops.export_mesh.stl(filepath=new_stl_file)
